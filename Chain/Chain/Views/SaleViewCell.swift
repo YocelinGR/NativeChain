969589e4577870d51798f517a15992df59cadc42
Yocelin Garcia <garcia_romero.y@hotmail.com>
2021-09-16 15:15:21 -0500

    Add the basic view files

---
+++ Chain/Chain/Views/SaleViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SaleViewCell.swift
+//  Chain
+//
+//  Created by Yocelin Garcia Romero on 16/09/21.
+//
+
+import UIKit
+
+class SaleViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
